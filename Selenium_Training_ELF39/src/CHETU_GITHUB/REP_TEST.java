9948612009944ca1c518356daf41cab79ca63da9
User <User@DESKTOP-PT84KK4>
2022-11-24 20:39:34 +0530

    Adding sopln statement

--- Selenium_Training_ELF39/src/CHETU_GITHUB/REP_TEST.java
+++ Selenium_Training_ELF39/src/CHETU_GITHUB/REP_TEST.java
@@ -1,8 +1,9 @@
-package SANKU_GITHUB;
+package CHETU_GITHUB;
 
 public class REP_TEST {
 	public static void main(String[] args) {
 		System.out.println("helllo sanku");
+		System.out.println("sanku manku");
 	}
 
 }
